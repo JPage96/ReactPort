5736b8e7e03c9b1ce6618e99de9e038d589c0c8a
JPage96 <justice.page1@pepsico.com>
2022-09-08 08:48:20 -0500

    display issues

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,21 +5,16 @@ function About() {
     return (
         <section id="about">
             <div classname="row">
-                <div className="nine columns main-col">
+                <div>
                     <h2>About Me</h2>
                     <p>Novice coder living in the midwest!
                     </p>
                     <div className="row">
-                        <div className="columns contact-details">
+                        <div className="contact-details">
                             <h2>Contact Me!</h2>
                             <p className="address">
                                 <span>Justice Page</span><br />
                                 <span>Justice.Page1@pepsico.com</span>
-                            </p>
-                        </div>
-                        <div className="columns download">
-                            <p>
-                                <a href={myResume} className="button"><i className="fa fa-download"></i>Download Resume</a>
                             </p>
                         </div>
                     </div>

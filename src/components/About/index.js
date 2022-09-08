84c886e313e23dc2b0e45cf320abf38a906892ee
JPage96 <justice.page1@pepsico.com>
2022-09-08 09:31:08 -0500

    spacing issues

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,13 +3,12 @@ import React from 'react';
 function About() {
     return (
         <section id="about">
-            <div classname="row">
+            <div>
                 <div>
                     <h2>About Me</h2>
                     <p>Novice coder living in the midwest!
                     </p>
-                    <div className="row">
-                        <div className="contact-details">
+                    <div>
                             <h2>Contact Me!</h2>
                             <p className="address">
                                 <span>Justice Page</span><br />
@@ -18,8 +17,8 @@ function About() {
                         </div>
                     </div>
                 </div>
-            </div>
         </section>
+        
     )
 }
 
